145c7777ed1bb2c9d2f4385ef687428a1a2d9bc4
Ubuntu <ubuntu@fv42iacdto6ikmsl2rqn.auto.internal>
2026-04-14 09:58:17 +0000

    op

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,20 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_file = BASE_DIR / '.env'
-if env_file.exists():
-    with open(env_file, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                key, value = line.split('=', 1)
-                os.environ[key] = value
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = False
 
-DEBUG = os.getenv('DEBUG')
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['158.160.253.252', '127.0.0.1', 'localhost', 'kattatty.serveblog.net']
 
 # Application definition
 
